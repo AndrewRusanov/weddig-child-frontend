cb07be0c9db4bc4d8a15a724162b1e826c24561a
Andrew Rusanov <rusanow.andrej2018@yandex.ru>
2025-09-05 22:08:27 +0300

    change base name

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
-const base = process.env.BASE_PATH || '/'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base,
+  base: '/weddig-child-frontend/',
 })
